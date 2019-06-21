cff6ad6a02a8a92f4a3e48e7c6bc1235aea9eeb9
Wesley Cramblitt <wcramblitt@gmail.com>
2019-06-21 01:10:13 -0600

    splitting the repo into front end and backend

---
+++ GainzWebAPI/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace GainzWebAPI
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var configurations = new ConfigurationBuilder()
+                .AddCommandLine(args)
+                .Build();
+
+            new WebHostBuilder()
+                .UseConfiguration(configurations)
+                .UseKestrel()
+                .UseContentRoot(Directory.GetCurrentDirectory())
+                .UseStartup<Startup>()
+                .Build()
+                .Run();
+
+        }
+
+    }
+}
